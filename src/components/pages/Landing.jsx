72758949329e3cb2ef69df53a2e63fafeed9ed46
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2024-04-20 16:20:02 +0800

    update landing

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,12 +1,21 @@
-import { Box, Center, IconButton, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  IconButton,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
-import { FaCompactDisc } from "react-icons/fa";
-import { Fade } from "react-awesome-reveal";
+// import { IoPlayCircle } from "react-icons/io5";
+import { FaPlay, FaPause } from "react-icons/fa";
+import { FaCompactDisc } from "react-icons/fa6";
+import { BsFillDiscFill } from "react-icons/bs";
 import Landing1 from "../../assets/landing-1.png";
 import Landing2 from "../../assets/landing-2.png";
 import Landing3 from "../../assets/covers.png";
 import song from "../../assets/song.mp3";
-import Dates from "./Dates";
 import Event from "./Event";
 import Gallery from "./Gallery";
 import Person1 from "./Person1";
@@ -15,73 +24,45 @@ import Person3 from "./Person3";
 import Gift from "./Gift";
 import Quotes from "./Quotes";
 import Family from "./Family";
+import Dates from "./Dates";
 
 const images = [Landing1, Landing2, Landing3];
-
-const preloadImage = (url) => {
-  const img = new Image();
-  img.src = url;
-};
 
 const Landing = () => {
   const [isPlaying, setIsPlaying] = useState(true);
   const audioRef = useRef();
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   useEffect(() => {
     if (isPlaying) {
       audioRef.current.play().catch((error) => {
+        // Tangani kesalahan saat memutar audio
         console.error("Audio playback error:", error);
       });
     } else {
       audioRef.current.pause();
     }
   }, [isPlaying]);
-
   const toggleAudio = () => {
     setIsPlaying((prevIsPlaying) => !prevIsPlaying);
   };
-
-  useEffect(() => {
-    const nextIndex =
-      currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1;
-    const prevIndex =
-      currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1;
-
-    preloadImage(images[nextIndex]);
-    preloadImage(images[prevIndex]);
-  }, [currentImageIndex]);
-
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentImageIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
-      setIsImageLoaded(false);
     }, 3000);
 
     return () => clearInterval(intervalId);
   }, [currentImageIndex]);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = images[currentImageIndex];
-    img.onload = () => {
-      setIsImageLoaded(true);
-      document.querySelector(".image-transition").classList.add("loaded");
-    };
-  }, [currentImageIndex]);
-
   return (
     <>
       <Box position="relative">
         <IconButton
           position="fixed"
-          bottom={20}
-          left={20}
+          bottom={5}
+          left={5}
           zIndex="1000"
-          padding={"6px"}
           icon={
             isPlaying ? (
               <FaCompactDisc size="48px" color="#413F42" />
@@ -101,20 +82,21 @@ const Landing = () => {
         <audio ref={audioRef} preload="auto" autoPlay={true} loop>
           <source src={song} type="audio/mpeg" />
         </audio>
-        <div
-          className={`h-[100vh] max-w-[438px] bg-cover bg-center bg-no-repeat desktop:w-[438px] px-[30px] relative image-transition ${
-            currentImageIndex === 0 ? "loaded" : ""
-          } ${isImageLoaded ? "opacity-100" : "opacity-0"}`}
+        <Box
+          w={{ base: "100vw", sm: "100vw", md: "438px", lg: "438px" }}
+          backgroundImage={`url(${images[currentImageIndex]})`}
+          backgroundSize={"cover"}
+          backgroundPosition={"center"}
+          backgroundRepeat={"no-repeat"}
+          h={"800px"}
+          py={"20px"}
+          px={"30px"}
+          position={"relative"}
+          color={"#212730"}
           style={{
-            backgroundImage: `url(${images[currentImageIndex]})`,
-            transition:
-              "background-image 1s ease-in-out, opacity 1s ease-in-out",
+            transition: "background-image 1s ease-in-out",
           }}
         >
-          <div
-            className="preload-image"
-            style={{ backgroundImage: `url(${images[0]})` }}
-          ></div>
           <Center>
             <Box
               position="absolute"
@@ -122,43 +104,35 @@ const Landing = () => {
               textAlign={"center"}
               color={"white"}
             >
-              <Fade duration={2000} direction="down" cascade={true}>
-                <div className="font-beauty text-4xl mb-4">
-                  Pengislaman & Penamatan
-                </div>
-                <div className="text-[12px] mt-1">
-                  <Fade duration={2000} direction="down" cascade={true}>
-                    <ul className="gap-4 space-y-1 font-bodyy mt-1">
-                      <li>Alhabsyi Abdullah Kaimuddin Dg Gading</li>
-                      <li>Muhammad Raffasya Alfarizqi Dg Tombong</li>
-                      <li>Nur Fauziyah Aprilia Dg Carammeng</li>
-                    </ul>
-                  </Fade>
-                </div>
-                <Text
-                  fontSize={{ base: "24px", lg: "32px" }}
-                  lineHeight={"60px"}
-                  fontWeight={"bold"}
-                ></Text>
-                <div className="mt-2 italic font-bodyy text-xl font-bold">
-                  Minggu,19 Mei 2024
-                </div>
-                <Box
-                  px={"20px"}
-                  minWidth={"320px"}
-                  maxWidth={"438px"}
-                  py={"20px"}
-                >
-                  <Text fontSize={"10px"} fontWeight={"bold"}>
-                    &quot;Semoga ini menjadi berkah bagi anak dan keluarga, dan
-                    semoga sedekah yang dikeluarkan menjadi penyelamat keluarga
-                    di akhirat nanti.&quot;
-                  </Text>
-                </Box>
-              </Fade>
+              <div className="font-beauty text-4xl mb-4">
+                Pengislaman & Penamatan
+              </div>
+              <div className="text-[12px] mt-1">
+                <ul className="gap-4 space-y-1 font-bodyy mt-1">
+                  <li>Alhabsyi Abdullah Kaimuddin Dg Gading</li>
+                  <li>Muhammad Raffasya Alfarizqi Dg Tombong</li>
+                  <li>Nur Fauziyah Aprilia Dg Carammeng</li>
+                </ul>
+              </div>
+              <div className="mt-2 italic font-bodyy text-xl font-bold">
+                Minggu,19 Mei 2024
+              </div>
+
+              <Box
+                px={"20px"}
+                minWidth={"320px"}
+                maxWidth={"438px"}
+                py={"20px"}
+              >
+                <Text fontSize={"10px"} fontWeight={"bold"}>
+                  &quot;Semoga ini menjadi berkah bagi anak dan keluarga, dan
+                  semoga sedekah yang dikeluarkan menjadi penyelamat keluarga di
+                  akhirat nanti.&quot;
+                </Text>
+              </Box>
             </Box>
           </Center>
-        </div>
+        </Box>
         <Dates />
         <Person1 />
         <Person2 />
